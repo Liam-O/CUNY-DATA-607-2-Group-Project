33bf84f893903d241c00e496d9099ad999b23d28
Luis Calleja <lecn99@hotmail.com>
2016-10-19 23:32:00 -0400

    appended to draw skill matches from DB

--- wordcountSuper.R
+++ wordcountSuper.R
@@ -1,5 +1,7 @@
 #source('wordcount.R')
 if (!require('stringr')) install.packages('stringr')
+if (!require('dplyr')) install.packages('dplyr')
+if (!require('tidyr')) install.packages('tidyr')
 
 # let's directly map our required list.   if wordcount.R gets us our 
 # frequency counts in an unsupervised way, wordcountSuper.R basically just
@@ -11,8 +13,11 @@ search <- function(term, df) {
 }
 
 # read in raw
-#raw <- scan('ScrapeOutput/scrapeOutput.txt', what="character", encoding='UTF-8', sep='\n')
-raw <- readLines('ScrapeOutput/scrapeOutput.txt', encoding = "ASCII")
+raw <- scan('ScrapeOutput/scrapeOutput.txt', what="character", encoding='UTF-8', sep='\n')
+
+#read in supervised skill set:
+sup.skills<-read.csv('/skills_lists.csv',stringsAsFactors = FALSE)
+names(sup.skills)<-'skill.name'
 
 # init clean
 raw <- str_replace_all(raw,"[^[:graph:]]", " ") 
@@ -20,7 +25,7 @@ raw <- tolower(raw)
 
 # need function to a) pull skills from db, b) make lowercase, 
 # c) put into a single vector called 'skills'
-skills <- c('java', 'r', 'scala', 'python', 'c', 'c++', 'sas', 'natural language processing')
+skills <- sup.skills
 
 ### search and create df
 mlist <- list()
@@ -30,6 +35,21 @@ for (i in 1:length(skills)) {
     count <- search(skill, raw)
     mlist[[i]] <- c(skill, count)
 }
+
+######### above loop did not work for me - going with sapply #############
+skill.freq<-sapply(sup.skills$skill.name,search,df=raw)
+# convert to df for easier analysis
+skill.freq.df<-as.data.frame(cbind(names(skill.freq),skill.freq),stringsAsFactors = FALSE, make.row.name=FALSE)
+
+#reveals that skills 'r', 'c' and 'c++' need to be addressed 
+skill.freq.df %>%
+  mutate(skill.freq=as.numeric(skill.freq)) %>%
+  arrange(desc(skill.freq)) %>%
+  head()
+
+#need to put spaces around the skills-from-DB vector... use that as a search
+
+#########  end of my rewrite  #############
 
 df <- do.call(rbind, mlist)
 colnames(df) <- c('Keyword', 'Freq')
