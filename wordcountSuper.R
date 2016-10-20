85c2fc09f224d6383ec8f8f8f804a1520fa43e37
wwells <walt@occ-data.org>
2016-10-20 07:01:05 -0500

    feat(supervised): luis code review, update small word to prep for scrape regex, clean house

--- wordcountSuper.R
+++ wordcountSuper.R
@@ -13,45 +13,39 @@ search <- function(term, df) {
 }
 
 # read in raw
-raw <- scan('ScrapeOutput/scrapeOutput.txt', what="character", encoding='UTF-8', sep='\n')
+#raw <- scan('ScrapeOutput/scrapeOutput.txt', what="character", encoding='UTF-8', sep='\n')
+raw <- readLines('ScrapeOutput/scrapeOutput.txt', encoding = "ASCII")
 
 #read in supervised skill set:
-sup.skills<-read.csv('/skills_lists.csv',stringsAsFactors = FALSE)
+sup.skills<-read.csv('skills_lists.csv',stringsAsFactors = FALSE)
 names(sup.skills)<-'skill.name'
 
 # init clean
 raw <- str_replace_all(raw,"[^[:graph:]]", " ") 
 raw <- tolower(raw)
 
-# need function to a) pull skills from db, b) make lowercase, 
-# c) put into a single vector called 'skills'
-skills <- sup.skills
+## quick convert r, c++, c
+sup.skills$skill.name[sup.skills$skill.name == "r"] <- "_r_"
+sup.skills$skill.name[sup.skills$skill.name == "c++"] <- "_cpp_"
+sup.skills$skill.name[sup.skills$skill.name == "c"] <- "_c_"
 
-### search and create df
-mlist <- list()
-
-for (i in 1:length(skills)) {
-    skill <- skills[i]
-    count <- search(skill, raw)
-    mlist[[i]] <- c(skill, count)
-}
-
-######### above loop did not work for me - going with sapply #############
+# apply search function to list of skills, create append results to raw
 skill.freq<-sapply(sup.skills$skill.name,search,df=raw)
 # convert to df for easier analysis
 skill.freq.df<-as.data.frame(cbind(names(skill.freq),skill.freq),stringsAsFactors = FALSE, make.row.name=FALSE)
 
-#reveals that skills 'r', 'c' and 'c++' need to be addressed 
+# order df 
 skill.freq.df %>%
   mutate(skill.freq=as.numeric(skill.freq)) %>%
-  arrange(desc(skill.freq)) %>%
-  head()
+  arrange(desc(skill.freq)) 
 
-#need to put spaces around the skills-from-DB vector... use that as a search
+names(skill.freq.df) <- c("Keyword", "Freq")
+row.names(skill.freq.df) <- c()
 
-#########  end of my rewrite  #############
+# convert back
+skill.freq.df$Keyword[skill.freq.df$Keyword == "_r_"] <- "r"
+skill.freq.df$Keyword[skill.freq.df$Keyword == "_cpp_"] <- "c++"
+skill.freq.df$Keyword[skill.freq.df$Keyword == "_c_"] <- "c"
 
-df <- do.call(rbind, mlist)
-colnames(df) <- c('Keyword', 'Freq')
-
-write.csv(df, "wordcountSuper.csv", row.names=FALSE)
+# export csv
+write.csv(skill.freq.df, "wordcountSuper.csv", row.names=FALSE)
